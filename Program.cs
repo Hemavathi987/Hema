ef7c6c795aeffee6e5b969b5edb0549c81503ede
Hemavathi987 <136674928+Hemavathi987@users.noreply.github.com>
2024-06-29 11:57:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Program
+{
+    class Demo
+    {
+        static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your age");
+        String name = Console.ReadLine();
+    Console.WriteLine("Enter Your DOB");
+        int age = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter your Address");
+        String add = Console.ReadLine();
+    Console.WriteLine("My name=" + name);
+        Console.WriteLine("My age=" + age);
+        Console.WriteLine("My address=" + add);
+            
+            String SUBString = add.Substring(add.IndexOf("a"));
+            Console.WriteLine("Final SubString====="+SUBString);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
